ae99c8d093aca3edf026a06cd021f0146d2d856f
Alan <alan@bowhouse.co.uk>
2015-10-07 17:12:07 +0100

    Add new files
    Upload all basic files

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Perch test</title>
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+    <!--[if lte IE 8]>
+      <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-old-ie-min.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+      <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
+    <!--<![endif]-->
+    <link rel="stylesheet" href="style.css" media="screen" charset="utf-8">
+  </head>
+  <body>
+    <div class="container">
+      <div class="box">      
+        <div class="pure-g">
+          <div class="pure-u-1-3"><p>Thirds</p></div>
+          <div class="pure-u-1-3"><p>Thirds</p></div>
+          <div class="pure-u-1-3"><p>Thirds</p></div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
